f5ab19040f2221603d86ca8e3931ed556f2b842e
ENOIIA <2668566027@qq.com>
2025-07-22 09:48:11 +0800

    refactor: 将活动类别的名字和步长重写为枚举类型

---
+++ src/utils/event_type.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class EventType(Enum):
+    CHALLENGE_LIVE = ("challenge_live", 50000)
+    VS_LIVE = ("vs_live", 6500)
+    LIVE_GOALS = ("live_goals", 26000)
+    MISSION_LIVE = ("mission_live", 15000)
+    TEAM_LIVE_FESTIVAL = ("team_live_festival", 6500)
+    MEDLEY_LIVE = ("medley_live", 18500)
+
+    @property
+    def score_step(self) -> int:
+        return self.value[1]
+
+    @property
+    def type_name(self) -> str:
+        return self.value[0]
