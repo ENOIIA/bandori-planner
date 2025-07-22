48abba8eb3f677988c1135e5237492744c14e76f
ENOIIA <2668566027@qq.com>
2025-07-22 09:49:20 +0800

    refactor: 使用枚举类型重构条件判断

--- src/utils/calculate_score.py
+++ src/utils/calculate_score.py
@@ -1,4 +1,5 @@
 from math import floor
+from src.utils.event_type import EventType
 
 
 # cp活（非歌榜）
@@ -38,19 +39,18 @@ def medley_live_pt(score: int) -> int:
 
 
 # 综合
-def calc_pt_per_game(score: int, bonus: float, support_band: int, event_type: str) -> int:
-
-    if event_type == "challenge_live":
+def calc_pt_per_game(score: int, bonus: float, support_band: int, event_type: EventType) -> int:
+    if event_type == EventType.CHALLENGE_LIVE:
         return challenge_live_pt(score, bonus)
-    elif event_type == "vs_live":
+    elif event_type == EventType.VS_LIVE:
         return vs_live_pt(score)
-    elif event_type == "live_goals":
+    elif event_type == EventType.LIVE_GOALS:
         return live_goals_pt(score, bonus)
-    elif event_type == "mission_live":
+    elif event_type == EventType.MISSION_LIVE:
         return mission_live_pt(score, bonus, support_band)
-    elif event_type == "team_live_festival":
+    elif event_type == EventType.TEAM_LIVE_FESTIVAL:
         return team_live_festival_pt(score)
-    elif event_type == "medley_live":
+    elif event_type == EventType.MEDLEY_LIVE:
         return medley_live_pt(score)
     else:
         raise RuntimeError("未知的活动类型！")
